e65f91ab5455fdd11e1e40250c86b072b863d74e
dsamuel32 <diego.samuel.alves@gmail.com>
2017-04-15 00:16:37 -0300

    criando test protractor

--- test/video.e2e.js
+++ test/video.e2e.js
@@ -1,9 +1,12 @@
 describe('Testing Video Player Component', function () {
 
-    it('Should be play video', function () {
+    it('Should be load component with video', function () {
         browser.get('demo/index.html');
 
-        var component = element(by.id('playerComponent'));
+        var video = element(by.tagName('video'));
+        var srcEsperado = 'http://ie.microsoft.com/testdrive/ieblog/2011/nov/pp4_blog_demo.mp4';
+        expect(srcEsperado).toContain(video.getAttribute('src'));
         
     });
+    
 });
